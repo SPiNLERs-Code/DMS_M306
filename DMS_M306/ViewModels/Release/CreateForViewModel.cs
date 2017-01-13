81cf1688b9912024f75f1b630cf0a26101f36e86
bryan albrecht <bryan.albrecht@noser.com>
2017-01-13 10:10:15 +0100

    Commiteted "release" files

---
+++ DMS_M306/ViewModels/Release/CreateForViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DMS_M306.ViewModels.Release
+{
+    public class CreateForViewModel
+    {
+        [Required]
+        public int FileId { get; set; }
+
+        public string FileName { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+    }
+}
