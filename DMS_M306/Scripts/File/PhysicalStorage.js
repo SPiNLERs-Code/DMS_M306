0c5aed1a34141ae1fa547ff3304e8573bc6cc165
bryan albrecht <bryan.albrecht@noser.com>
2017-01-18 11:56:41 +0100

    Fixed PhysicalStorage javascript

--- DMS_M306/Scripts/File/PhysicalStorage.js
+++ DMS_M306/Scripts/File/PhysicalStorage.js
@@ -2,13 +2,20 @@
 if($("#physicalStorageSelction").val() != 1)
 {
     $("#physicalStorageInfos").hide();
+    $("#fileToUploadFields").show();
+} else {
+    $("#physicalStorageInfos").show();
+    $("#fileToUploadFields").hide();
 }
 
 $("#physicalStorageSelction").change(function () {
 
     if ($("#physicalStorageSelction").val() == 1) {
         $("#physicalStorageInfos").show("slow");
+        $("#fileToUploadFields").hide("slow");
     } else {
         $("#physicalStorageInfos").hide("slow");
+        $("#fileToUploadFields").show("slow");
     }
 })
+
