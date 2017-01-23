69260635732429db14725a881d4d741970e41bce
bryan albrecht <bryan.albrecht@noser.com>
2017-01-23 15:07:52 +0100

    Added Scan Logic

---
+++ DMS_M306/Controllers/ScanController.cs
@@ -0,0 +1,53 @@
+﻿using DMS_M306.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DMS_M306.Controllers
+{
+    public class ScanController : Controller
+    {
+        private IFileRepository _fileRepository;
+
+        public ScanController(IFileRepository fileRepository)
+        {
+            _fileRepository = fileRepository;
+        }
+
+        // GET: Scan
+        public ActionResult Index(string ScanString)
+        {
+            if (ScanString == "" && ScanString != null) return RedirectToAction("Index","Home");
+            var spilted = ScanString.Split('-');
+            if(spilted.Count() != 2) return RedirectToAction("Index", "Home");
+
+            int FileID = 0;
+            int ReleaseNumber = 0;
+
+
+            if(int.TryParse(spilted[0], out FileID)&& int.TryParse(spilted[1],out ReleaseNumber))
+            {
+                var file = _fileRepository.Get(x => x.Id == FileID).FirstOrDefault();
+                if(file != null)
+                {
+                    if(ReleaseNumber == 0)
+                    {
+                        return RedirectToAction("Details", "File", new { @Id = FileID });
+                    }
+                    if(file.Releases != null)
+                    {
+                        var release = file.Releases.Where(x => x.ReleaseNumber == ReleaseNumber).FirstOrDefault();
+                        if(release != null)
+                        {
+                            return RedirectToAction("Details", "FileRelease", new { @Id = release.Id });
+                        }
+                    }
+
+                }
+            }
+                return RedirectToAction("Index", "Home");
+        }
+    }
+}
--- DMS_M306/Helpers/QRCodeHelper.cs
+++ DMS_M306/Helpers/QRCodeHelper.cs
@@ -31,7 +31,7 @@ namespace DMS_M306.Helpers
 
         public static IHtmlString GenerateFileCode(this HtmlHelper html, int fileId)
         {
-            string releaseDeafault = "00000000";
+            string releaseDeafault = "0000";
             var hexString = fileId.ToString("X6");
             string qrString = hexString + "-" + releaseDeafault;
             return GenerateQrCode(html, qrString, "DMS-File-QR", 400, 1);
